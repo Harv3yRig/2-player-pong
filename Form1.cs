b0b0f158f0f04600118245866731457c2917988b
Harv3yRig <166531099+Harv3yRig@users.noreply.github.com>
2024-09-06 13:24:21 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,7 @@ namespace Pong
             public static int player1Movement = 0, player2Movement = 0;
             public static int player1Score = 0, player2Score = 0;
             public static int verticalMovement = 0;
-            public static int counter = 0;
+            public static int gameTime = 3;
         }
 
         public Form1()
@@ -22,6 +22,7 @@ namespace Pong
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            setVisibility(false, true);
         }
 
         public void Form_KeyDown(object sender, KeyEventArgs e)
@@ -37,7 +38,6 @@ namespace Pong
 
             if (e.KeyCode == Keys.Up && rightPlayer.Top > 0)
             {
-
                 globalVariables.player2Movement = -globalVariables.playerSpeed;
             }
             else if (e.KeyCode == Keys.Down && rightPlayer.Bottom < this.ClientSize.Height)
@@ -48,8 +48,13 @@ namespace Pong
 
         private void Form_KeyUp(object sender, KeyEventArgs e)
         {
-            globalVariables.player2Movement = 0;
-            globalVariables.player1Movement = 0;
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.S)
+            {
+                globalVariables.player1Movement = 0;
+            } else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                globalVariables.player2Movement = 0;
+            }
         }
 
         private void ClockTimer_Tick(object sender, EventArgs e)
@@ -66,7 +71,7 @@ namespace Pong
             else if (ball.Left <= 0)
             {
                 globalVariables.player2Score += 1;
-                player1Score.Text = $"Score: {globalVariables.player2Score}";
+                player2Score.Text = $"Score: {globalVariables.player2Score}";
                 ResetBallPosition();
                 globalVariables.ballSpeed = -1;
                 globalVariables.verticalMovement = 0;
@@ -116,32 +121,31 @@ namespace Pong
             }
 
             //winning detection
-            if (globalVariables.player1Score == 10)
+            if (globalVariables.player1Score == globalVariables.gameTime)
             {
                 winner.Text = "PLAYER 1 WINS";
-                clockTimer.Enabled = false;
-                startGame.Enabled = true;
-                startGame.Visible = true;
-                startGame.Text = "Restart game?";
+                EndGame();
             }
-            else if (globalVariables.player2Score == 10)
+            else if (globalVariables.player2Score == globalVariables.gameTime)
             {
                 winner.Text = "PLAYER 2 WINS";
-                clockTimer.Enabled = false;
-                startGame.Enabled = true;
-                startGame.Visible = true;
+                EndGame();
             }
         }
 
         private void StartGame_Click(object sender, EventArgs e)
         {
-                clockTimer.Enabled = true;
-                startGame.Enabled = false;
-                startGame.Visible = false;
+                setVisibility(true, false);
+            if (amountOfGames.Value != 0)
+            {
+                globalVariables.gameTime = (int)amountOfGames.Value;
+            }
                 leftPlayer.Top = (this.ClientSize.Height) / 2;
                 rightPlayer.Top = (this.ClientSize.Height) / 2;
                 globalVariables.player1Score = 0;
                 globalVariables.player2Score = 0;
+                player1Score.Text = $"Score: {globalVariables.player1Score}";
+                player2Score.Text = $"Score: {globalVariables.player2Score}";
                 globalVariables.verticalMovement = 0;
                 winner.Text = "";
         }
@@ -149,6 +153,12 @@ namespace Pong
         {
             ball.Left = (this.ClientSize.Width - ball.Width) / 2;
             ball.Top = (this.ClientSize.Height - ball.Height) / 2;
+        }
+
+        private void EndGame()
+        {
+            setVisibility(false, true);
+            startGame.Text = "Restart game?";
         }
         private int calculateHitPosition(int aboveSpace, int belowSpace)
         {
@@ -160,6 +170,20 @@ namespace Pong
             {
                 return belowSpace;
             }
+        }
+        private void setVisibility(bool visible1, bool visible2)
+        {
+            clockTimer.Enabled = visible1;
+            rightPlayer.Visible = visible1;
+            leftPlayer.Visible = visible1;
+            ball.Visible = visible1;
+            player1Score.Visible = visible1;
+            player2Score.Visible = visible1;
+            gameLabel.Visible = visible2;
+            startGame.Enabled = visible2;
+            startGame.Visible = visible2;
+            amountOfGames.Visible = visible2;
+            amountOfGames.Enabled = visible2;
         }
     }
 }
